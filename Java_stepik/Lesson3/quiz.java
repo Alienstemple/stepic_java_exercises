71faf4251d848569474371fa5772ef7094916eb8
Alyona <borisovsk0101@gmail.com>
2020-01-04 19:51:47 +0300

    Initial commit

---
+++ Java_stepik/Lesson3/quiz.java
@@ -0,0 +1,10 @@
+public class quiz {
+    public final int a=9;
+    private static final int b = 7;
+    public int mul(int c) {return a*c;}
+    public quiz () {}
+    public static void main (String [] args) {
+        System.out.println("rarar!");
+    }
+
+}
