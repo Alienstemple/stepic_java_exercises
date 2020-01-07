13d068a8d3bbab252cdd643fd09cb1bcdf00b926
Alyona <borisovsk0101@gmail.com>
2020-01-07 20:28:34 +0300

    logging

--- Java_stepik/Lesson4/Logging.java
+++ Java_stepik/Lesson4/Logging.java
@@ -1,15 +1,14 @@
 import java.util.logging.*;
 private static void configureLogging() {
-    Logger A = Logger.getLogger("org.stepic.java.logging.ClassA");  // создадим два логера (для начала)
+    Logger A = Logger.getLogger("org.stepic.java.logging.ClassA");  // создадим два логгера (для начала)
     Logger B = Logger.getLogger("org.stepic.java.logging.ClassB");
     
     A.setLevel(Level.ALL);     // установим уровни логирования для обоих в соотв с ТЗ
     B.setLevel(Level.WARNING);
 
     // создадим логгер для классов из org.stepic.java
-    Logger HIGH_CLASS_LOGGER = Logger.getLogger("org.stepic.java"); // или просто org.stepic.java
+    Logger HIGH_CLASS_LOGGER = Logger.getLogger("org.stepic.java"); 
                   // запретим передавать сообщения вышестоящ. логгерам
-                  // или понадобится setLevel ALL
     HIGH_CLASS_LOGGER.setUseParentHandlers(false);   
                // создадим ConsoleHandler и привяжем к нему логгер
     ConsoleHandler HANDLER = new ConsoleHandler();
