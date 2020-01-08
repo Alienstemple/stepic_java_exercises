165a18b500eaf57699d918c5ccbac7291e07e7a9
Alyona <borisovsk0101@gmail.com>
2020-01-08 20:30:17 +0300

    started to work with "MailSystem" task

---
+++ Java_stepik/Lesson4/MailSystem/src/com/BorisovskayaINC/Main.java
@@ -0,0 +1,12 @@
+package com.BorisovskayaINC;
+
+
+public class Main {
+    public static final String AUSTIN_POWERS = "Austin Powers";
+    public static final String WEAPONS = "weapons";
+    public static final String BANNED_SUBSTANCE = "banned substance";
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
