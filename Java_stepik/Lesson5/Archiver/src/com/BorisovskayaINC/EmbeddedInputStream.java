0bacc0b1b6e4193ee046d96333dbd74539f4b5cb
Alyona <borisovsk0101@gmail.com>
2020-01-31 12:38:56 +0300

    Archiver

--- Java_stepik/Lesson5/Archiver/src/com/BorisovskayaINC/EmbeddedInputStream.java
+++ Java_stepik/Lesson5/Archiver/src/com/BorisovskayaINC/EmbeddedInputStream.java
@@ -22,10 +22,50 @@ public class EmbeddedInputStream {
         this.bufferLimit = 0;
     }
 
-    @Override
+    //@Override
     public int read() throws IOException {
         byte[] bytes = new byte[1];
         int bytesRead = read(bytes);       // TODO: ???
         return bytesRead == 1 ? bytes[0] : IOStatus.EOF;
+    }
+
+    //@Override
+    public int read(byte[] bytes) throws IOException{
+        return read(bytes, 0, bytes.length);
+    }
+
+    //@Override
+    public int read(byte[] bytes, int offset, int length) throws IOException {
+        int bytesRead = 0;
+        while (0 < length) {
+            if (isBufferEmpty() && readBufferFromUnderlyingStream() <= 0) { // TODO: как реализован?
+                break;
+            }
+            int bytesGotFromBuffer = getBytesFromBuffer(bytes, offset, length); // TODO: как реализован?
+            bytesRead += bytesGotFromBuffer;
+            offset += bytesGotFromBuffer;
+            length -= bytesGotFromBuffer;
+        }
+        return bytesRead;
+    }
+
+    //@Override
+    public long skip(long bytesToSkip) throws IOException {
+        for (long i = 0; i < bytesToSkip; ++i) {
+            if (read() == IOStatus.EOF) {
+                return i;
+            }
+        }
+    }
+
+    private boolean isBufferFull() { return bufferPosition == buffer.length; }
+
+    private boolean isBufferEmpty() { return bufferPosition == 0; }
+
+    private int readBufferFromUnderlyingStream() {
+        return 0; // TODO: как реализован?
+    }
+    private int getBytesFromBuffer(byte[] bytes, int offset, int length) {
+        return 0; // TODO: как реализован?
     }
 }
